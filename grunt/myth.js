4e8c3471a9767ba8df48bac64a4ac32e0e77d710
Derek Dennison <dbdennison@ucdavis.edu>
2017-01-21 02:36:57 -0800

    Use Myth

--- grunt/myth.js
+++ grunt/myth.js
@@ -2,7 +2,6 @@ module.exports = {
 	dist: {
 		files: {
 			'dist/css/styles.css': 'src/css/styles.css',
-			'dist/css/calendar.css': 'src/css/calendar.css',
 			'dist/css/normalize.css': 'src/css/normalize.css',
 		}
 	}
