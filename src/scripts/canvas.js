862571f6f557bfe2b651050df085dd266a78864a
Derek Dennison <dbdennison@ucdavis.edu>
2017-01-23 00:01:35 -0800

    Grunt and HTML setup

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,15 +2,20 @@ console.log("map canvas ready");
 
 var canvas = document.querySelector('.map-canvas');
 var ctx = canvas.getContext('2d');
-var imageObj = new Image();
+var baseMap = new Image();
+var titleMap = new Image();
 
 canvas.width = 1000;
 canvas.height = 667;
 
-imageObj.onload = draw;
-window.onresize = draw;
-imageObj.src = 'img/basemap/base_magellan.jpg';
+window.onload = draw;
+//window.onresize = draw;
+
+baseMap.src = 'img/basemap/base_magellan.jpg';
+titleMap.src = 'img/basemap/title_magellan.svg';
 
 function draw(){
-	ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
-};
+
+	ctx.drawImage(baseMap, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(titleMap, 0, 0, canvas.width, canvas.height);
+}
